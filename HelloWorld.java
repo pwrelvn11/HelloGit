53d0f4c00336a52e47d25df14984f2358ef83958
pwrelvn11 <sskmaho193@yahoo.co.jp>
2019-06-09 13:06:54 +0900

    最初のコミット

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+/***********************************************************
+	‚g‚…‚Œ‚Œ‚I‚v‚‚’‚Œ‚„
+************************************************************/
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello! Java World.");	// 'S' ‚Í‘å•¶š
+	}
+}
